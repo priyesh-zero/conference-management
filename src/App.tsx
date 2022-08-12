d6793380fcb36daed412cac1b091da4ee4e652c0
priyesh-zero <priyesh.zero@gmail.com>
2022-08-12 19:48:13 +0530

    🎨 Tailwind CSS configured
        On branch develop
    
        Changes to be committed:
    
            - modified:   package.json
    
            - new file:   postcss.config.cjs
    
            - modified:   src/App.tsx
    
            - deleted:    src/assets/react.svg
    
            - modified:   src/main.tsx
    
            - new file:   src/styles/main.scss
    
            - new file:   tailwind.config.cjs
    
            - modified:   tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { TestComponent } from "components/test-component";
+
 export const App = () => {
     return (
         <div className="App">
-            <h1>Conference Management</h1>
+            <h1 className="text-5xl font-bold">Conference Management</h1>
         </div>
     );
 };
